a8d60bde0e234248cf8a9cbbb7a05e9e02e76d2c
mementosss <144599371+mementosss@users.noreply.github.com>
2024-03-22 09:05:06 +0300

    Edited parse

--- parse.js
+++ parse.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 // Usage: node parse.js
-//
+// int
 // Script that creates index.html out of web/template.html and README.md.
 //
 // It is written in JS because this code used to be executed on the client side.
